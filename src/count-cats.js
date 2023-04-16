0874e5de9659288dbf779b1ac4ed150379c40103
IvanLenkavets <126865917+IvanLenkavets@users.noreply.github.com>
2023-04-16 20:54:57 +0300

    fix: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-let count = 0;
-
 function countCats(matrix) {
+  let count = 0;
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] === '^^') {
@@ -26,6 +25,7 @@ function countCats(matrix) {
   }
   return count;
 }
+
 
 
 module.exports = {
