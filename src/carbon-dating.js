5f35abb39b5904a1d59604b6eaec2430eda66379
IvanLenkavets <126865917+IvanLenkavets@users.noreply.github.com>
2023-04-16 20:50:16 +0300

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity ) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  let activity = parseFloat(sampleActivity);
+  const count = 0.693 / HALF_LIFE_PERIOD;
+  const age = Math.round(Math.log(MODERN_ACTIVITY/ activity)/count +0.5);
+  if (isNaN(age)) {
+    return false;
+  }
+  if ( activity <= 0 || activity > MODERN_ACTIVITY  ) {
+    return false;
+  }
+  return age;
 }
 
 module.exports = {
